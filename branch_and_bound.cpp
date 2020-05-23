511cd1420fbfca7b7a88af236c668c95533a0f63
Matteo Soo <matteosoo11@gmail.com>
2020-05-23 18:44:10 +0800

    revise the algorithm
    ref: https://www.techiedelight.com/travelling-salesman-problem-using-branch-and-bound/

--- branch_and_bound.cpp
+++ branch_and_bound.cpp
@@ -1,219 +1,275 @@
-// C++ program to solve Traveling Salesman Problem 
-// using Branch and Bound. 
-#include <bits/stdc++.h> 
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <cstring>
+#include <climits>
 #include <chrono> 
+#include <fstream>
 using namespace std::chrono;
-using namespace std; 
+using namespace std;
 
-int n;
+int N;
+// Sentinal value for representing infinity
+#define INF INT_MAX
 
-// final_path[] stores the final solution ie, the 
-// path of the salesman. 
-vector<int> final_path; //[n+1]
+vector<int> solution_list; 
 
-// visited[] keeps track of the already visited nodes 
-// in a particular path 
-vector<bool> visited; //[n]
+// State Space Tree nodes
+struct Node
+{
+	// stores edges of state space tree
+	// helps in tracing path when answer is found
+	vector<pair<int, int>> path;
 
-// Stores the final minimum weight of shortest tour. 
-int final_res = INT_MAX; 
+	// stores the reduced matrix
+	// int reducedMatrix[N][N];
+	vector<vector<int>> reducedMatrix;
 
-// Function to copy temporary solution to 
-// the final solution 
-void copyToFinal(int curr_path[]) 
-{ 
-    // reset final_path first 
-    final_path.erase(final_path.begin(),final_path.end());
-    // then update the current path to final path
-	for (int i=0; i<n; i++)
-		final_path.push_back(curr_path[i]); 
-	final_path.push_back(curr_path[0]);
-} 
+	// stores the lower bound
+	int cost;
 
-// Function to find the minimum edge cost 
-// having an end at the vertex i 
-int firstMin(vector<vector<int>>& adj, int i) 
-{ 
-	int min = INT_MAX; 
-	for (int k=0; k<n; k++) 
-		if (adj[i][k]<min && i != k) 
-			min = adj[i][k]; 
-	return min; 
-} 
+	//stores current city number
+	int vertex;
 
-// function to find the second minimum edge cost 
-// having an end at the vertex i 
-int secondMin(vector<vector<int>>& adj, int i) 
-{ 
-	int first = INT_MAX, second = INT_MAX; 
-	for (int j=0; j<n; j++) 
-	{ 
-		if (i == j) 
-			continue; 
+	// stores number of cities visited so far
+	int level;
+};
 
-		if (adj[i][j] <= first) 
-		{ 
-			second = first; 
-			first = adj[i][j]; 
-		} 
-		else if (adj[i][j] <= second && 
-				adj[i][j] != first) 
-			second = adj[i][j]; 
-	} 
-	return second; 
-} 
+// Function to allocate a new node (i, j) corresponds to visiting
+// city j from city i
+Node* newNode(vector<vector<int>>& parentMatrix, vector<pair<int, int>> const &path,
+			int level, int i, int j)
+{
+	Node* node = new Node;
 
-// function that takes as arguments: 
-// curr_bound -> lower bound of the root node 
-// curr_weight-> stores the weight of the path so far 
-// level-> current level while moving in the search 
-//		 space tree 
-// curr_path[] -> where the solution is being stored which 
-//			 would later be copied to final_path[] 
-void TSPRec(vector<vector<int>>& adj, int curr_bound, int curr_weight, 
-			int level, int curr_path[]) 
-{ 
-	// base case is when we have reached level N which 
-	// means we have covered all the nodes once 
-	if (level==n) 
+	// stores ancestors edges of state space tree
+	node->path = path;
+	// skip for root node
+	if (level != 0)
+		// add current edge to path
+		node->path.push_back(make_pair(i, j));
+	
+	// copy data from parent node to current node
+	// memcpy(node->reducedMatrix, parentMatrix,
+	// 	sizeof node->reducedMatrix);
+	node->reducedMatrix = parentMatrix;
+	
+	// Change all entries of row i and column j to infinity
+	// skip for root node
+	for (int k = 0; level != 0 && k < N; k++)
 	{
-		// check if there is an edge from last vertex in 
-		// path back to the first vertex 
-		if (adj[curr_path[level-1]][curr_path[0]] != 0) 
-		{ 
-			// curr_res has the total weight of the 
-			// solution we got 
-			int curr_res = curr_weight + 
-					adj[curr_path[level-1]][curr_path[0]]; 
+		// set outgoing edges for city i to infinity
+		node->reducedMatrix[i][k] = INF;
 
-			// Update final result and final path if 
-			// current result is better. 
-			if (curr_res < final_res) 
-			{ 
-				copyToFinal(curr_path); 
-				final_res = curr_res; 
-			} 
-		} 
-		return; 
-	} 
+		// set incoming edges to city j to infinity
+		node->reducedMatrix[k][j] = INF;
+	}
+	
+	// Set (j, 0) to infinity
+	// here start node is 0
+	node->reducedMatrix[j].at(0) = INF;
 
-	// for any other level iterate for all vertices to 
-	// build the search space tree recursively 
-	for (int i=0; i<n; i++) 
-	{ 
-		// Consider next vertex if it is not same (diagonal 
-		// entry in adjacency matrix and not visited 
-		// already) 
-		if (adj[curr_path[level-1]][i] != 0 && 
-			visited[i] == false) 
-		{ 
-			int temp = curr_bound; 
-			curr_weight += adj[curr_path[level-1]][i]; 
-            // cout << curr_weight << endl;
+	// set number of cities visited so far
+	node->level = level;
 
-			// different computation of curr_bound for 
-			// level 2 from the other levels 
-			if (level==1) 
-			curr_bound -= ((firstMin(adj, curr_path[level-1]) + 
-							firstMin(adj, i))/2); 
-			else
-			curr_bound -= ((secondMin(adj, curr_path[level-1]) + 
-							firstMin(adj, i))/2); 
+	// assign current city number
+	node->vertex = j;
 
-			// curr_bound + curr_weight is the actual lower bound 
-			// for the node that we have arrived on 
-			// If current lower bound < final_res, we need to explore 
-			// the node further 
-			if (curr_bound + curr_weight < final_res) 
-			{ 
-				curr_path[level] = i; 
-				// visited[i] = true;
-                visited.at(i) = true;
+	// return node
+	return node;
+}
 
-				// call TSPRec for the next level 
-				TSPRec(adj, curr_bound, curr_weight, level+1, 
-					curr_path); 
-			} 
+// Function to reduce each row in such a way that
+// there must be at least one zero in each row
+void rowReduction(vector<vector<int>>& reducedMatrix, vector<int>& row)
+{
+	// initialize row array to INF
+	// fill_n(row, N, INF);
+	for (int i=0; i<N; i++)
+		row.push_back(INF);
 
-			// Else we have to prune the node by resetting 
-			// all changes to curr_weight and curr_bound 
-			curr_weight -= adj[curr_path[level-1]][i]; 
-			curr_bound = temp; 
-            
-			// Also reset the visited array 
-			// memset(visited, false, sizeof(visited));
-            visited.erase(visited.begin(), visited.end()); 
-            for (int i = 0; i < n+1; i++)
-                visited.push_back(0);
-			for (int j=0; j<=level-1; j++) 
-                visited.at(curr_path[j]) = true;
-				// visited[curr_path[j]] = true;
-		} 
-	} 
-} 
+	// row[i] contains minimum in row i
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (reducedMatrix[i][j] < row[i])
+				row.at(i) = reducedMatrix[i][j];
 
-// This function sets up final_path[] 
-void TSP(vector<vector<int>>& adj) 
-{ 
-	int curr_path[n+1]; 
+	// reduce the minimum value from each element in each row
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (reducedMatrix[i][j] != INF && row[i] != INF)
+				reducedMatrix[i][j] -= row[i];
+}
 
-	// Calculate initial lower bound for the root node 
-	// using the formula 1/2 * (sum of first min + 
-	// second min) for all edges. 
-	// Also initialize the curr_path and visited array 
-	int curr_bound = 0; 
-	memset(curr_path, -1, sizeof(curr_path)); 
-	// memset(visited, 0, sizeof(curr_path));
-    // std::fill(visited.begin(), visited.begin()+n+1, 0);
-    for (int i = 0; i < n+1; i++)
-        visited.push_back(0);
+// Function to reduce each column in such a way that
+// there must be at least one zero in each column
+void columnReduction(vector<vector<int>>& reducedMatrix, vector<int>& col)
+{
+	// initialize col array to INF
+	// fill_n(col, N, INF);
+	for (int i=0; i<N; i++)
+		col.push_back(INF);
 
-	// Compute initial bound 
-	for (int i=0; i<n; i++) 
-		curr_bound += (firstMin(adj, i) + 
-					secondMin(adj, i));
+	// col[j] contains minimum in col j
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (reducedMatrix[i][j] < col[j])
+				col.at(j) = reducedMatrix[i][j];
 
-	// Rounding off the lower bound to an integer 
-	curr_bound = (curr_bound&1)? curr_bound/2 + 1 : 
-								curr_bound/2; 
-    
-	// We start at vertex 1 so the first vertex 
-	// in curr_path[] is 0
-    // visited[0] = true;  
-    visited.at(0) = true;
-	curr_path[0] = 0; 
+	// reduce the minimum value from each element in each column
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			if (reducedMatrix[i][j] != INF && col[j] != INF)
+				reducedMatrix[i][j] -= col[j];
+}
 
-	// Call to TSPRec for curr_weight equal to 
-	// 0 and level 1 
-	TSPRec(adj, curr_bound, 0, 1, curr_path); 
-} 
+// Function to get the lower bound on
+// on the path starting at current min node
+int calculateCost(vector<vector<int>>& reducedMatrix)
+{
+	// initialize cost to 0
+	int cost = 0;
 
-// Driver code 
-int main(int argc, char* argv[]) 
-{ 
-    freopen(argv[1], "r", stdin);
-	// size_t n;
-    std::cin >> n;
+	// Row Reduction
+	// int row[N];
+	vector<int> row;
+	rowReduction(reducedMatrix, row);
 
-    vector<vector<int>> adj(n, vector<int>(n));
+	// Column Reduction
+	// int col[N];
+	vector<int> col;
+	columnReduction(reducedMatrix, col);
 
-	for (size_t i = 0; i < n; i++)
-		for (size_t j = 0; j < n; j++)
-			std::cin >> adj[i][j];
+	// the total expected cost
+	// is the sum of all reductions
+	for (int i = 0; i < N; i++)
+		cost += (row[i] != INT_MAX) ? row[i] : 0,
+			cost += (col[i] != INT_MAX) ? col[i] : 0;
 
-    auto start = high_resolution_clock::now();
-	TSP(adj);
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
+	return cost;
+}
 
-    // write file to BF.txt
+// print list of cities visited following least cost
+void printPath(vector<pair<int, int>> const &list)
+{
+	for (int i = 0; i < list.size(); i++)
+		solution_list.push_back(list[i].first);
+		// cout << list[i].first << " - " << list[i].second << endl;
+}
+
+// Comparison object to be used to order the heap
+struct comp {
+	bool operator()(const Node* lhs, const Node* rhs) const
+	{
+		return lhs->cost > rhs->cost;
+	}
+};
+
+// Function to solve Traveling Salesman Problem using Branch and Bound
+int solve(vector<vector<int>>&  costMatrix)
+{
+	// Create a priority queue to store live nodes of search tree;
+	priority_queue<Node*, std::vector<Node*>, comp> pq;
+
+	vector<pair<int, int>> v;
+	
+	// create a root node and calculate its cost
+	// The TSP starts from first city i.e. node 0
+	Node* root = newNode(costMatrix, v, 0, -1, 0);
+	
+	// get the lower bound of the path starting at node 0
+	root->cost = calculateCost(root->reducedMatrix);
+
+	// Add root to list of live nodes;
+	pq.push(root);
+
+	// Finds a live node with least cost, add its children to list of
+	// live nodes and finally deletes it from the list
+	while (!pq.empty())
+	{
+		// Find a live node with least estimated cost
+		Node* min = pq.top();
+
+		// The found node is deleted from the list of live nodes
+		pq.pop();
+
+		// i stores current city number
+		int i = min->vertex;
+
+		// if all cities are visited
+		if (min->level == N - 1)
+		{
+			// return to starting city
+			min->path.push_back(make_pair(i, 0));
+
+			// print list of cities visited;
+			printPath(min->path);
+
+			// return optimal cost
+			return min->cost;
+		}
+
+		// do for each child of min
+		// (i, j) forms an edge in space tree
+		for (int j = 0; j < N; j++)
+		{
+			if (min->reducedMatrix[i][j] != INF)
+			{
+				// create a child node and calculate its cost
+				Node* child = newNode(min->reducedMatrix, min->path,
+					min->level + 1, i, j);
+
+				/* Cost of the child =
+					cost of parent node +
+					cost of the edge(i, j) +
+					lower bound of the path starting at node j
+				*/
+				child->cost = min->cost + min->reducedMatrix[i][j]
+							+ calculateCost(child->reducedMatrix);
+
+				// Add child to list of live nodes
+				pq.push(child);
+			}
+		}
+
+		// free node as we have already stored edges (i, j) in vector.
+		// So no need for parent node while printing solution.
+		delete min;
+	}
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	freopen(argv[1], "r", stdin);
+	std::cin >> N;
+
+	vector<vector<int>> costMatrix(N, vector<int>(N));
+
+	for (size_t i = 0; i < N; i++){
+		for (size_t j = 0; j < N; j++){
+			if (costMatrix[i][j] == -1){
+				costMatrix[i][j] = INF;
+			}
+			std::cin >> costMatrix[i][j];
+		}
+	}
+
+	auto start = high_resolution_clock::now();
+	int cost = solve(costMatrix);
+	auto stop = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>(stop - start);
+	
+	// write file to BB.txt
     ofstream output_file("BB.txt");
     output_file << "Solution : " ; 
-    for (int i=0; i<n; i++) 
-	    output_file << final_path[i] << " - ";
+    for (int i = 0; i < solution_list.size(); i++) 
+        output_file << solution_list[i] << " - "; 
     output_file << "0" << endl;
-    output_file << "Cost     : " << final_res << endl;
-    output_file << "Time     : " << (duration.count())/(1e+6) << " s" << endl;
+	output_file << "Cost     : " << cost << endl; 
+	output_file << "Time     : " << (duration.count())/(1e+6) << " s" << endl;
     output_file.close(); 
-	return 0; 
-} 
+
+	return 0;
+}
